1ca73918027bfa4b9d466173f500c670d2714342
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-03-12 16:37:20 +0300

    Add: create LoginView

--- Messenger/Core/Authentication/View/LoginView.swift
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -8,8 +8,110 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                //logo image
+                Image(.logoMain)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                //textfields
+                TextField("Enter your email", text: $email)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
+                
+                SecureField("Enter your password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
+                
+                //forgot password
+                NavigationLink {
+                    Text("Reset password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                //login button
+                Button(action: {
+                    print("login tap")
+                }, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(.blue)
+                        .cornerRadius(10)
+                })
+                .padding(.vertical)
+
+                //separator
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundStyle(.gray)
+                
+                //sign with google
+                HStack(spacing: 0) {
+                    Image(.logoGoogle)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 30, height: 30)
+                    
+                    Button(action: {
+                        print("login with hoogle")
+                    }, label: {
+                        Text("Continue with Google")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                    })
+                }
+                
+                Spacer()
+                
+                //sign up link
+                Divider()
+                NavigationLink {
+                    Text("Sign Up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+            }
+        }
     }
 }
 
