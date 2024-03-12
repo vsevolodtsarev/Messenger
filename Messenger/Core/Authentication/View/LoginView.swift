0bde0bf70153826e5ed2699309ebc786600df0bb
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-03-12 15:38:15 +0300

    Update project scheme

---
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Всеволод Царев on 12.03.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
