79aa3e5841f8bf3f6989a3004a6ac19b19a8d14d
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-03-24 18:51:30 +0300

    Add: Create NewMessageView

---
+++ Messenger/Core/NewMessage/NewMessageView.swift
@@ -0,0 +1,65 @@
+//
+//  NewMessageView.swift
+//  Messenger
+//
+//  Created by Всеволод Царев on 24.03.2024.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                TextField("Search", text: $searchText)
+                    .frame(height: 44)
+                    .padding(.leading)
+                    .background(Color(.systemGroupedBackground))
+                
+                Text("CONTACTS")
+                    .foregroundStyle(.gray)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ForEach(0 ... 10, id: \.self) { user in
+                    VStack {
+                        HStack {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            .foregroundStyle(Color(.systemGray4))
+                            
+                            Text(verbatim: "Test User")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
+                        }
+                        .padding(.leading)
+                        
+                        Divider()
+                            .padding(.leading, 40)
+                    }
+                }
+            }
+            .navigationTitle("New Message")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+        NewMessageView()
+}
