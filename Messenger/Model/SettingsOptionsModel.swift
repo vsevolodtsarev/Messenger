a39e35c3b2fd6c7d3aaffe6bcdb3e3e72c7ee6a5
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-03-25 22:05:43 +0300

    Add: create SettingsOptionsModel

---
+++ Messenger/Model/SettingsOptionsModel.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsOptionsModel.swift
+//  Messenger
+//
+//  Created by Всеволод Царев on 25.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum SettingsOptionsModel: Int, CaseIterable, Identifiable {
+    var id: Int { return self.rawValue }
+    
+    case darkMode
+    case activeStatus
+    case accessibility
+    case privacy
+    case notifications
+    
+    var title: LocalizedStringKey {
+        switch self {
+        case .darkMode:
+            "Dark Mode"
+        case .activeStatus:
+            "Active Status"
+        case .accessibility:
+            "Accessibility"
+        case .privacy:
+            "Privacy"
+        case .notifications:
+            "Notifications"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .darkMode:
+            return "moon.circle.fill"
+        case .activeStatus:
+            return "message.badge.circle.fill"
+        case .accessibility:
+            return "person.circle.fill"
+        case .privacy:
+            return "lock.circle.fill"
+        case .notifications:
+            return "bell.circle.fill"
+        }
+    }
+    
+    var imageColor: Color {
+        switch self {
+            
+        case .darkMode:
+                .black
+        case .activeStatus:
+                .green
+        case .accessibility:
+                .black
+        case .privacy:
+                .blue
+        case .notifications:
+                .purple
+        }
+    }
+}
