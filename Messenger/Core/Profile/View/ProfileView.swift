9eafbb62a69ad024e44405f9b62fc21910fbb330
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-02 11:08:51 +0300

    Update: implement signOut function

--- Messenger/Core/Profile/View/ProfileView.swift
+++ Messenger/Core/Profile/View/ProfileView.swift
@@ -52,7 +52,7 @@ struct ProfileView: View {
                 
                 Section {
                     Button("Log Out") {
-                        print("log out")
+                        AuthService.shared.signOut()
                     }
                     
                     Button("Delete Account") {
