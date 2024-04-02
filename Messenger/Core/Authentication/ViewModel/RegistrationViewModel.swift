26c451d7e5c0635267b90dffd6e6cd37c4c4db3c
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-02 11:07:55 +0300

    Add: create RegistrationViewModel

--- Messenger/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ Messenger/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -5,4 +5,14 @@
 //  Created by Всеволод Царев on 02.04.2024.
 //
 
-import Foundation
+import SwiftUI
+
+final class RegistrationViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var fullname = ""
+    
+    func createUser() async throws {
+        try await AuthService.shared.createUser(withEMail: email, password: password, fullname: fullname)
+    }
+}
