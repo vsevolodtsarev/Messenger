6edbedfb9511314f467e23ae9616c857dafad4d1
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-02 11:09:12 +0300

    Add: create ContentViewModel

--- Messenger/Core/Root/ViewModel/ContentViewModel.swift
+++ Messenger/Core/Root/ViewModel/ContentViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by Всеволод Царев on 02.04.2024.
 //
 
-import Foundation
+import Firebase
+import Combine
+
+final class ContentViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        AuthService.shared.$userSession.sink { [weak self] userSessionFromAuthService in
+            guard let self else { return }
+            self.userSession = userSessionFromAuthService
+        }.store(in: &cancellables)
+    }
+}
