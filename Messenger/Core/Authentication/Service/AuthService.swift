5ae50ac382769a113bb1f69cb119cd82a9778992
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-02 11:06:15 +0300

    Add: create AuthService

--- Messenger/Core/Authentication/Service/AuthService.swift
+++ Messenger/Core/Authentication/Service/AuthService.swift
@@ -5,4 +5,47 @@
 //  Created by Всеволод Царев on 02.04.2024.
 //
 
-import Foundation
+import Firebase
+
+final class AuthService {
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        userSession = Auth.auth().currentUser
+        print("User session with user id \(userSession?.uid)")
+    }
+    
+    func login(withEMail email: String, password: String) async throws {
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            userSession = result.user
+        } catch {
+            print("Failed to sign in with error: \(error.localizedDescription)")
+        }
+    }
+    
+    func login(withGoogle email: String) async throws {
+        print("login with google")
+    }
+    
+    func createUser(withEMail email: String, password: String, fullname: String) async throws {
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            userSession = result.user
+            print("Create user: \(result.user.uid)")
+        } catch {
+            print("Failed to create user with error: \(error.localizedDescription)")
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            userSession = nil
+        } catch {
+            print("Failed to Sign out with errje: \(error.localizedDescription)")
+        }
+    }
+}
