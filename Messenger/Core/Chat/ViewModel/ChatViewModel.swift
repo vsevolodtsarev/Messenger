f96c85514674b0e99d55c70443a5a2848edf5b18
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-15 17:48:55 +0300

    Update: implement function for observe messages

--- Messenger/Core/Chat/ViewModel/ChatViewModel.swift
+++ Messenger/Core/Chat/ViewModel/ChatViewModel.swift
@@ -9,13 +9,22 @@ import Foundation
 
 final class ChatViewModel: ObservableObject {
     @Published var messageText = ""
+    @Published var messages = [Message]()
     let user: User
     
     init(user: User) {
         self.user = user
+        observeMessages()
     }
     
     func sendMessage() {
         MessageService.sendMessage(messageText, toUser: user)
+    }
+    
+    func observeMessages() {
+        MessageService.observeMessages(chatPartner: user) { [weak self] messages in
+            guard let self else { return }
+            self.messages.append(contentsOf: messages)
+        }
     }
 }
