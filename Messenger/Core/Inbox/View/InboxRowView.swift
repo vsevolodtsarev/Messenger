ccf5a83f7b1774db492c4218a057a13f49a2b58b
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-19 17:50:16 +0300

    Update: add real user data

--- Messenger/Core/Inbox/View/InboxRowView.swift
+++ Messenger/Core/Inbox/View/InboxRowView.swift
@@ -8,16 +8,18 @@
 import SwiftUI
 
 struct InboxRowView: View {
+    let message: Message
+    
     var body: some View {
         HStack(alignment: .top, spacing: 12) {
-            ImageProfileView(user: User.mockUser, size: .medium)
+            ImageProfileView(user: message.user, size: .medium)
             
             VStack(alignment: .leading, spacing: 4) {
-                Text(verbatim: "Test Name")
+                Text(verbatim: message.user?.fullname ?? "")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                 
-                Text(verbatim: "Some last message was here with two lines")
+                Text(verbatim: message.messageText)
                     .font(.subheadline)
                     .foregroundStyle(.gray)
                     .lineLimit(2)
@@ -34,8 +36,4 @@ struct InboxRowView: View {
         }
         .frame(height: 72)
     }
-}
-
-#Preview {
-    InboxRowView()
 }
