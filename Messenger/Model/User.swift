f9b681d842458f348fee9a89ae6f3c989f87e8b1
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-29 13:41:11 +0300

    Add: firstName variable

--- Messenger/Model/User.swift
+++ Messenger/Model/User.swift
@@ -17,6 +17,12 @@ struct User: Codable, Hashable, Identifiable {
     var id: String {
         return uid ?? UUID().uuidString
     }
+    
+    var firstName: String {
+        let formatter = PersonNameComponentsFormatter()
+        let components = formatter.personNameComponents(from: fullname)
+        return components?.givenName ?? fullname
+    }
 }
 
 extension User {
