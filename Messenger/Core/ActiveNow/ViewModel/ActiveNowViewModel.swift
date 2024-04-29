de8ed9b0245310ae810b9901661137a3354be09f
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-04-29 13:20:45 +0300

    Add: create ActiveNowViewModel

---
+++ Messenger/Core/ActiveNow/ViewModel/ActiveNowViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveNowViewModel.swift
+//  Messenger
+//
+//  Created by Всеволод Царев on 29.04.2024.
+//
+
+import Foundation
+
+final class ActiveNowViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    init() {
+        Task { try await fetchUsers() }
+    }
+    
+    func fetchUsers() async throws {
+        users = try await UserService.fetchAllUsers()
+    }
+}
